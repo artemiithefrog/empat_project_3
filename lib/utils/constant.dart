12a85ccdc006e0aec0e62ae65702d8451dc1437f
Artemii <artemiithefrog95@icloud.com>
2024-03-17 15:05:13 +0100

    created constant

---
+++ lib/utils/constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const double defaultPadding = 16.0;
+const double defaultBorderRadius = 10.0;
+
+const TextStyle bigTitle =
+    TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold);
+const TextStyle normalTitle =
+    TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold);
+const TextStyle titleStyle =
+    TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600);
+const TextStyle subTitleStyle =
+    TextStyle(color: Colors.white, fontWeight: FontWeight.w500);
